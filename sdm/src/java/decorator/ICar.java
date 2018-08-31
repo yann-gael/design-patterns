09cc2ca362216a9705c2829c451ac5be489d44e0
Yann-Gaël <yann-gael.gueheneuc@polymtl.ca>
2018-08-31 15:31:56 +0000

    Update ICar.java

--- sdm/src/java/decorator/ICar.java
+++ sdm/src/java/decorator/ICar.java
@@ -1,6 +1,6 @@
 package src.java.decorator;
 
-public interface ICar {
+public interface ICar { // Somehow, ICar should use OptionalExtra to allow the decoration of any car
     ICar addOption();
     void showCar();
 }
