db6f949edf0b31dfbd30f261e7703c9d41b42af0
Yann-Gaël <yann-gael.gueheneuc@polymtl.ca>
2018-08-31 14:57:32 +0000

    Update Limbs.java

--- sdm/src/java/association/Limbs.java
+++ sdm/src/java/association/Limbs.java
@@ -6,7 +6,7 @@ public class Limbs {
     {
         this.count = count;
     }
-    void showAll()
+    void showAll() // Talking about a common superclass, the show/showAll method should be declared in the superclass so that an algorithm can print information without having to know with which body part it is dealing. 
     {
         System.out.println("Limbs count: " + count);
     }
