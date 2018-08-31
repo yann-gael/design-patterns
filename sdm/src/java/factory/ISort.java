5d40e863a57b89182d7f7c7ead7e447ee25c3ecf
Yann-Gaël <yann-gael.gueheneuc@polymtl.ca>
2018-08-31 15:38:57 +0000

    Update ISort.java

--- sdm/src/java/factory/ISort.java
+++ sdm/src/java/factory/ISort.java
@@ -1,5 +1,5 @@
-package src.java.factory;
-import static src.java.utilities.Utilities.print;
+package src.java.factory; // BTW, "src" should not be part of the package name of none of the code
+import static src.java.utilities.Utilities.print; // Remove
 public interface ISort
 {
     public void sort();
