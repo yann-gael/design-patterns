33399a1e39bf425d051c26ddabb86bffdcb46e12
Yann-Gaël <yann-gael.gueheneuc@polymtl.ca>
2018-08-31 15:03:01 +0000

    Update Person.java
    Class Person should also have some common superclass with limbs...

--- sdm/src/java/association/Person.java
+++ sdm/src/java/association/Person.java
@@ -2,11 +2,11 @@ package src.java.association;
 
 public class Person
 {
-    int count;
-    Head head;
-    Torso torso;
-    Limbs limbs;
-    Person(int headCount, int torsoCount, int limbsCount)
+    int count; // Dead code, use your IDE to identify and remove dead code.
+    Head head; // Multiple instances rather than one instance with a count.
+    Torso torso; // Same
+    Limbs limbs; // Same
+    Person(int headCount, int torsoCount, int limbsCount) // Why not public? Also remove the "long" list of int parameters
     {
         head = new Head(headCount);
         torso = new Torso(torsoCount);
@@ -14,7 +14,7 @@ public class Person
     }
     void showAll()
     {
-        System.out.println("New person created.");
+        System.out.println("New person created."); // Remove
         head.showAll();
         torso.showAll();
         limbs.showAll();
