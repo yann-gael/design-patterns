bb099a4b999cad0d87d6b1ee1b929ae24c950b1c
Yann-Gaël <yann-gael.gueheneuc@polymtl.ca>
2018-08-31 15:48:27 +0000

    Update ITicketIterator.java

--- sdm/src/java/iterator/ITicketIterator.java
+++ sdm/src/java/iterator/ITicketIterator.java
@@ -2,7 +2,7 @@ package src.java.iterator;
 
 import java.util.Iterator;
 
-public interface ITicketIterator extends Iterator<Integer>
+public interface ITicketIterator extends Iterator<Integer> // Is this interface really necessary? In any case you could keep it though!
 {
     boolean hasNext();
 
