b40e049e4acb5291b350750b213d5b18352d4823
Yann-Gaël <yann-gael.gueheneuc@polymtl.ca>
2018-08-31 16:14:11 +0000

    Update Singleton.java

--- sdm/src/java/singleton/Singleton.java
+++ sdm/src/java/singleton/Singleton.java
@@ -2,13 +2,13 @@ package src.java.singleton;
 import java.util.Random;
 
 public class Singleton {
-    int behaviour;
-    private static final Singleton new_singleton = new Singleton();
+    int behaviour; // Visibility
+    private static final Singleton new_singleton = new Singleton(); // NewSingleton or UniqueInstance
     private Singleton()
     {
-            Random rand = new Random();
-            behaviour = rand.nextInt(50);
-    }
+            Random rand = new Random(); // Remove
+            behaviour = rand.nextInt(50); // this.behaviour
+    } 
     public static Singleton getInstance()
     {
         return new_singleton;
