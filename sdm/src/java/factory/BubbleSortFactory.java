ac51f198ae948e7505657e64f891040742b4cf9e
Yann-Gaël <yann-gael.gueheneuc@polymtl.ca>
2018-08-31 15:46:21 +0000

    Update BubbleSortFactory.java
    Eclipse has an option to clean up / format whole projects at once, IntelliJ too?

--- sdm/src/java/factory/BubbleSortFactory.java
+++ sdm/src/java/factory/BubbleSortFactory.java
@@ -2,7 +2,7 @@ package src.java.factory;
 
 public class BubbleSortFactory implements AbstractSortFactory
 {
-
+// Should clean all the code to make it consistently formatted
     public ISort getSort() {
         BubbleSort sortObj = new BubbleSort();
         return sortObj;
